93a8b852c93418ed60090303dbe8224b80059415
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-09-24 23:19:34 -0300

    Correções gerais

--- client/prisma.config.ts
+++ client/prisma.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, env } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
+import 'dotenv/config';
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
